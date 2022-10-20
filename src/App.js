e6d1498e2711226f55751a5bb43b67a0ffcbaef5
Acer <yesartem7@gmail.com>
2022-10-20 20:10:30 +0300

    Chgghh

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => onToggleButton()}>xyi</button>
+      <button onClick={() => onToggleButton()}>Хуй</button>
     </div>
   );
 }
